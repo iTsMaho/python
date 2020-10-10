cf9aad88869ef2eb2b2500a59856e2fdc4e40437
iTsMaho <mahmut.celik57@icloud.com>
2020-10-10 17:31:44 +0200

    alo

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-import firstMD
+class MyClass:
+    zahl = 42
+    string = "zeichenkette"
+    list = []
 
-firstMD.msg("Hallo")
+    def __init__(self, buchstabeneu='a'):
+        self.buchstabe = buchstabeneu
+        self.list = []
+
+    def do_something(self, neuezahl):
+        self.zahl = neuezahl
+        self.list.append(neuezahl)
+
+instanz = MyClass('Z')
+instanz2 = MyClass('b')
+instanz.do_something(1337)
+print(instanz.list)
+
--- firstMD.py
+++
@@ -1,2 +0,0 @@
-def msg(msg):
-    print(msg)
